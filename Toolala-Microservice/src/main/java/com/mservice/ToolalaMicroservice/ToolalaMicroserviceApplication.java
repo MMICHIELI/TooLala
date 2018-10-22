cbed17714a59a9755661b0dfcc2e8269dbee91db
Marc MICHIELI <zyx1@free.fr>
2018-10-22 22:50:19 +0200

    Test Jgitflow

---
+++ Toolala-Microservice/src/main/java/com/mservice/ToolalaMicroservice/ToolalaMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.mservice.ToolalaMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolalaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolalaMicroserviceApplication.class, args);
+	}
+}
